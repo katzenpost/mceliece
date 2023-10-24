ae45067240687edd3b351756246ad18f523a34ec
David Stainton <dstainton415@gmail.com>
2023-10-23 20:45:41 -0700

    Add schemes.go file so it compiles again

---
+++ kem/schemes/schemes.go
@@ -0,0 +1,24 @@
+// Package schemes contains a register of KEM schemes.
+package schemes
+
+var allSchemes = [...]kem.Scheme{}
+
+var allSchemeNames map[string]kem.Scheme
+
+func init() {
+	allSchemeNames = make(map[string]kem.Scheme)
+	for _, scheme := range allSchemes {
+		allSchemeNames[strings.ToLower(scheme.Name())] = scheme
+	}
+}
+
+// ByName returns the scheme with the given name and nil if it is not
+// supported.
+//
+// Names are case insensitive.
+func ByName(name string) kem.Scheme {
+	return allSchemeNames[strings.ToLower(name)]
+}
+
+// All returns all KEM schemes supported.
+func All() []kem.Scheme { a := allSchemes; return a[:] }
